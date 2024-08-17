81e8fe8980e8ec3d4947e7386fb92f5772e09665
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-08-17 20:28:00 -0300

    adicionado Jwt para segurança, tempo de sessão. 1hr

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -1,13 +1,16 @@
+import "dotenv/config";
 
 import registrarEventosDocumento from "./resgistraEventos/Documento.js";
 import registrarEventosInicio from "./resgistraEventos/Inicio.js";
 import registrarEventosCadastro from "./resgistraEventos/Cadastro.js";
+import registrarEventosLogin from "./resgistraEventos/login.js";
+
 import io from "./servidor.js";
 
 io.on("connection", (socket) => {
 
+  registrarEventosCadastro(socket, io);
+  registrarEventosLogin(socket, io);
   registrarEventosInicio(socket, io);
   registrarEventosDocumento(socket, io);
-  registrarEventosCadastro(socket, io);
-
 });
